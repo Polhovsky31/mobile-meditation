743c32170887d1dbeab4179be3d06b22e21e6eaa
Polhovsky31 <71878410+Polhovsky31@users.noreply.github.com>
2022-12-20 10:07:17 +0300

    Add files via upload

---
+++ meditation P.N/meditation P.N/meditation P.N/MainViewController.swift
@@ -0,0 +1,27 @@
+//
+//  MainViewController.swift
+//  meditation K. N.
+//
+//  Created by Arslan Kurbanov on 10.09.2022.
+//
+
+import Foundation
+import Alamofire
+import SwiftyJSON
+import UIKit
+
+class MainViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
+    
+    @IBOutlet weak var userPicture: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
